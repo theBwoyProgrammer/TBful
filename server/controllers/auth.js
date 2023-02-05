79951858ebc46e96e6d6d77cc436b738a93e7589
Codeaddictjaver <102610715+HatimDev-HE@users.noreply.github.com>
2023-02-05 15:54:25 +0300

    login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -47,7 +47,9 @@ export const login = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if(!isMatch) return res.status(400).json({ message: "Invalid credentials" });
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({ token, user });
     } catch(error) {
-        res.status(500).json({ message: error.message });
+        res.status(500).json({ error: error.message });
     }
 }
