9d222c82e446ab87285fec1da6ff65b0aa5e8a58
Codeaddictjaver <102610715+HatimDev-HE@users.noreply.github.com>
2023-02-05 14:53:14 +0300

    routes

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import register from "./controllers/auth.js";
 
 // CONFIGURATION
 const __filename = fileURLToPath(import.meta.url);
@@ -35,7 +36,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // ROUTES
-app.post("/auth/register", upload.single("picture", register));
+app.post("/auth/register", upload.single("picture"), register);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001
