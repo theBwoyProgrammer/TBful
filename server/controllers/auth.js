70628af606762a6c83b2cb4e73e7b02f87dd2449
Codeaddictjaver <102610715+HatimDev-HE@users.noreply.github.com>
2023-02-05 15:50:23 +0300

    jwt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -36,4 +36,18 @@ export const register = async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
+};
+
+// LOGIN
+export const login = async (req, res) => {
+    try{
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if(!user) return res.status(400).json({ message: "User doesn't exist" });
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    } catch(error) {
+        res.status(500).json({ message: error.message });
+    }
 }
